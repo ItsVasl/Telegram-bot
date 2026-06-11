5793024335d1ae39ed9d807bf454faeaefe3d1b5
Its Vasl <113450033+ItsVasl@users.noreply.github.com>
2026-06-11 22:24:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,16 +8,34 @@ async def reply_with_gif(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message:
         return
 
-    text = update.message.text or ""
+    text = (update.message.text or "").lower()
 
-    if "?" in text:
+    triggers = {
+        "enti",
+        "ente",
+        "ask",
+        "emaindi",
+        "emaindhi",
+        "enduku",
+        "enduko",
+        "endhuko",
+        "endhi"
+    }
+
+    words = text.split()
+
+    if "?" in text or any(word in triggers for word in words):
         with open("just-asking-brahmi.mp4", "rb") as gif_file:
             await update.message.reply_animation(animation=gif_file)
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(
-    MessageHandler(filters.TEXT & ~filters.COMMAND, reply_with_gif)
+    MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        reply_with_gif
+    )
 )
 
+print("Bot is running...")
 app.run_polling()
